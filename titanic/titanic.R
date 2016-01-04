b63407e91d2766e13c1683f27176a937797759f3
Damian Piątkowski <damian.dem@gmail.com>
2016-01-04 11:10:23 +0100

    więcej tutoriali

--- titanic/titanic.R
+++ titanic/titanic.R
@@ -1,9 +1,6 @@
 setwd("G:/repo/kaggle-struggle/titanic")
 
-library(rpart)
-library(rattle)
-library(rpart.plot)
-library(RColorBrewer)
+library(party)
 
 train <- read.csv("G:/repo/kaggle-struggle/titanic/data/train.csv")
 test <- read.csv("G:/repo/kaggle-struggle/titanic/data/test.csv")
@@ -14,8 +11,9 @@ combi$Name <- as.character(combi$Name)
 
 combi$Title <- sapply(combi$Name, FUN=function(x) {strsplit(x, split='[,.]')[[1]][2]})
 combi$Title <- sub(' ', '', combi$Title)
-combi$Title[combi$Title %in% c('Mme', 'Mlle', 'Dona', 'Lady', 'Miss', 'Mrs', 'the Countess')] <- 'Ms'
-combi$Title[combi$Title %in% c('Capt', 'Col', 'Don', 'Dr', 'Jonkheer', 'Major', 'Master', 'Rev', 'Sir')] <- 'Mr'
+combi$Title[combi$Title %in% c('Mme', 'Mlle')] <- 'Mlle'
+combi$Title[combi$Title %in% c('Capt', 'Don', 'Major', 'Sir')] <- 'Sir'
+combi$Title[combi$Title %in% c('Dona', 'Lady', 'the Countess', 'Jonkheer')] <- 'Lady'
 combi$Title <- factor(combi$Title)
 
 combi$FamilySize <- combi$SibSp + combi$Parch + 1
@@ -28,12 +26,27 @@ famIDs <- famIDs[famIDs$Freq <= 2,]
 
 combi$FamilyID[combi$FamilyID %in% famIDs$Var1] <- 'Small'
 combi$FamilyID <- factor(combi$FamilyID)
+combi$FamilyID2 <- combi$FamilyID
+combi$FamilyID2 <- as.character(combi$FamilyID2)
+combi$FamilyID2[combi$FamilySize <= 3] <- 'Small'
+combi$FamilyID2 <- factor(combi$FamilyID2)
+
+Agefit <- rpart(Age ~ Pclass + Sex + SibSp + Parch + Fare + Embarked + Title + FamilySize, data=combi[!is.na(combi$Age),], method="anova")
+combi$Age[is.na(combi$Age)] <- predict(Agefit, combi[is.na(combi$Age),])
+
+combi$Embarked[c(62,830)] = "S"
+combi$Embarked <- factor(combi$Embarked)
+
+combi$Fare[1044] <- median(combi$Fare, na.rm=TRUE)
+
+set.seed(415)
 
 train <- combi[1:891,]
 test <- combi[892:1309,]
 
-fit <- rpart(Survived ~ Pclass + Sex + Age + SibSp + Parch + Fare + Embarked + Title + FamilySize + FamilyID, data=train, method="class")
+fit <- cforest(as.factor(Survived) ~ Pclass + Sex + Age + SibSp + Parch + Fare + Embarked + Title + FamilySize + FamilyID,
+               data = train, controls=cforest_unbiased(ntree=2000, mtry=3))
 
-Prediction <- predict(fit, test, type = "class")
+Prediction <- predict(fit, test, OOB=TRUE, type = "response")
 
 write.csv(data.frame(PassengerId = test$PassengerId, Survived = Prediction), file = "gone.csv", row.names = FALSE)
